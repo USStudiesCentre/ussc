925011c57245c19cd7118f80353f15e6a5868e81
Zoe Meers <zoe.meers@gmail.com>
2018-03-01 07:58:59 +1000

    update ussc package

--- R/theme_ussc.R
+++ R/theme_ussc.R
@@ -4,16 +4,14 @@
 
 # add the desired font to the font database (you can add multiple)
 ussc_fonts <- function(){
-  install.packages('showtext')
-  library('showtext')
-  font_add("univers", regular="fonts/Linotype - UniversLTPro-55Roman.otf", 
+  showtext::font_add("univers", regular="fonts/Linotype - UniversLTPro-55Roman.otf", 
          bold="fonts/Linotype - UniversLTPro-65Bold.otf",
          italic="fonts/Linotype - UniversLTPro-45LightOblique.otf")
-  font_add("universLight", regular="fonts/Linotype - UniversLTPro-45Light.otf")
-  font_add("neosanspro", regular="fonts/Neo Sans Pro.ttf",
+  showtext::font_add("universLight", regular="fonts/Linotype - UniversLTPro-45Light.otf")
+  showtext::font_add("neosanspro", regular="fonts/Neo Sans Pro.ttf",
          bold="fonts/Neo Sans Pro Bold.ttf",
          italic="Neo Sans Pro Italic.ttf")
-  font_add("neosansproLight", regular="fonts/Neo Sans Pro Light.ttf")
+  showtext::font_add("neosansproLight", regular="fonts/Neo Sans Pro Light.ttf")
 }
 
 #' Main USSC theme
