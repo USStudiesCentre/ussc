a7f999bd6ecaeda081d999c28541c489dac5c178
Zoe Meers <zoe.meers@gmail.com>
2018-03-01 12:55:43 +1000

    Update zzz.R

--- zzz.R
+++ zzz.R
@@ -5,5 +5,18 @@
 
 .onLoad <- function(libname, pkgname) {
   # something to run
-  ussc_fonts()
+  ussc_fonts <- function(){
+  univers <- quartzFont(paste("Univers LT Pro",
+                              c("65 Bold","45 Light Oblique","45 Light", "55 Roman")))
+  universLight <- quartzFont(paste("Univers LT Pro",
+                                   c("45 Light", "55 Roman","65 Bold","45 Light Oblique")))
+  neosanspro <- quartzFont(paste("Neo Sans Pro",
+                                 c("Bold", "Italic", "Regular", "Medium")))
+  neosansproLight <- quartzFont(paste("Neo Sans Pro",
+                                      c("Light", "Light Italic", "Regular", "Medium")))
+  quartzFonts(univers=univers)
+  quartzFonts(universLight=universLight)
+  quartzFonts(neosanspro=neosanspro)
+  quartzFonts(neosansproLight=neosansproLight)
+  }
 }
