ab696210890fa2e3acf78da3c7fc9ae71d14d584
Zoe Meers <zoe.meers@gmail.com>
2018-03-03 17:53:20 +1100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  library.dynam("ussc", pkgname, libname)
   ussc_fonts()
   }
-
-.onUnload <- function(libpath) {
-  library.dynam.unload("ussc", libpath)
-}
