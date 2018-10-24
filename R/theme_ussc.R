e429cf630e1d49311c6f81f8a41c4b8a7944317d
Zoe Meers <zoe.meers@sydney.edu.au>
2018-10-24 16:37:50 +1100

    add dark theme

--- R/theme_ussc.R
+++ R/theme_ussc.R
@@ -62,13 +62,68 @@ theme_ussc <- function() {
       legend.text = element_text(size = 11, "universLight"),
       legend.title = element_text(size = 10, "univers"),
       legend.key.size = unit(x = 10, units = "pt"),
-      plot.title = element_text(size = 14, family = "neosanspro"),
+      plot.title = element_text(size = 14, family = "univers"),
       plot.caption = element_text(size = 10, family = "universLight"),
       plot.subtitle = element_text(size = 11, family = "universLight"),
-      plot.background = element_blank(),
+      plot.background = element_rect(fill = "transparent", colour = NA),
+      panel.background = element_rect(fill = "transparent", colour = NA),
       strip.background = element_blank()
     )
 }
+
+
+#' Dark USSC theme
+#' @description
+#' Calls the dark USSC theme. A reconstructed theme_bw() ggplot2 theme.
+#' @usage
+#' theme_ussc_dark()
+#' @examples
+#' Create ggplot theme using dark USSC theme
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, colour = Species)) + geom_point(size = 4, alpha=0.4) +  theme_ussc_dark() + labs(title="Neo Sans Pro Header", x="Univers Font: Sepal Width", y="Univers Font: Sepal Length") + scale_colour_ussc("blue")
+#' @author
+#' Zoe Meers
+theme_ussc_dark <- function() {
+  theme_bw(base_family = "univers") +
+    theme(
+      # Specify axis options
+      axis.line = element_blank(),  
+      axis.text.x = element_text(size = 12*0.8, color = "white", lineheight = 0.9),  
+      axis.text.y = element_text(size = 12*0.8, color = "white", lineheight = 0.9),  
+      axis.ticks = element_line(color = "white", size  =  0.2),  
+      axis.title.x = element_text(size = 12, color = "white", margin = margin(0, 10, 0, 0)),  
+      axis.title.y = element_text(size = 12, color = "white", angle = 90, margin = margin(0, 10, 0, 0)),  
+      axis.ticks.length = unit(0.3, "lines"),   
+      # Specify legend options
+      legend.background = element_rect(color = NA, fill = "#2a2a2b"),  
+      legend.key = element_rect(color = NA,  fill = "#2a2a2b"),  
+      legend.key.size = unit(1.2, "lines"),  
+      legend.key.height = NULL,  
+      legend.key.width = NULL,      
+      legend.text = element_text(size = 12*0.8, color = "white"),  
+      legend.title = element_text(size = 12*0.8, face = "bold", hjust = 0, color = "white"),  
+      legend.position = "top",  
+      legend.text.align = NULL,  
+      legend.title.align = NULL,  
+      legend.direction = "horizontal",  
+      legend.box = NULL, 
+      # Specify panel options
+      panel.background = element_rect(fill = "#2a2a2b", color  =  NA),  
+      panel.border = element_blank(), ##element_rect(fill = NA, color = "white"),  
+      panel.grid.major = element_line(color = "grey35"),  
+      panel.grid.minor = element_line(color = "grey20"),  
+      panel.spacing = unit(0.5, "lines"),   
+      # Specify facetting options
+      strip.background = element_rect(fill = "grey30", color = "grey10"),  
+      strip.text.x = element_text(size = 12*0.8, color = "white"),  
+      strip.text.y = element_text(size = 12*0.8, color = "white",angle = -90),  
+      # Specify plot options
+      plot.background = element_rect(color = "#2a2a2b", fill = "#2a2a2b"),  
+      plot.title = element_text(size = 12*1.2, color = "white"),  
+      plot.margin = unit(rep(1, 4), "lines"),
+      plot.subtitle = element_text(size = 12*1.2, color = "white"),
+      plot.caption = element_text(size = 12*1.2, color = "white"))
+}
+
 #' USSC theme with Univers font
 #' @description
 #' A theme_bw() with Univers font
@@ -96,7 +151,8 @@ theme_ussc_univers <- function() {
       plot.title = element_text(size = 14),
       plot.caption = element_text(size = 10),
       plot.subtitle = element_text(size = 11),
-      plot.background = element_blank(),
+      plot.background = element_rect(fill = "transparent", colour = NA),
+      panel.background = element_rect(fill = "transparent", colour = NA),
       strip.background = element_blank()
     )
 }
@@ -128,7 +184,8 @@ theme_ussc_univers_light <- function() {
       plot.subtitle = element_text(size = 11),
       plot.title = element_text(size = 14),
       plot.caption = element_text(size = 10),
-      plot.background = element_blank(),
+      plot.background = element_rect(fill = "transparent", colour = NA),
+      panel.background = element_rect(fill = "transparent", colour = NA),
       strip.background = element_blank()
     )
 }
@@ -160,7 +217,8 @@ theme_ussc_neosanspro <- function() {
       legend.key.size = unit(x = 9, units = "pt"),
       plot.title = element_text(size = 14),
       plot.subtitle = element_text(size = 10, family = "neosansproLight"),
-      plot.background = element_blank(),
+      plot.background = element_rect(fill = "transparent", colour = NA),
+      panel.background = element_rect(fill = "transparent", colour = NA),
       strip.background = element_blank()
     )
 }
@@ -193,7 +251,8 @@ theme_ussc_neosanspro_light <- function() {
       plot.title = element_text(size = 14),
       plot.subtitle = element_text(size = 10),
       plot.caption = element_text(size = 10),
-      plot.background = element_blank(),
+      plot.background = element_rect(fill = "transparent", colour = NA),
+      panel.background = element_rect(fill = "transparent", colour = NA),
       strip.background = element_blank()
     )
 }
