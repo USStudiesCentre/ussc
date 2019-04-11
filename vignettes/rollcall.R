aa7cf0fb6270913eadedacd918ba3d5fce6bb234
Zoe Meers <zoe.meers@gmail.com>
2019-04-12 09:06:31 +1000

    update docs

--- vignettes/rollcall.R
+++
@@ -1,7 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, warnings=FALSE, message=FALSE)
-library(r2d3)
-library(here)
-library(ussc)
-#set wd or open the file directly. 
-
