b70976598d7d3f28efdeb8e4b4dabdcb3f053aa1
Zoe Meers <zoe.meers@gmail.com>
2019-10-02 12:32:05 +1000

    add img folder

--- R/plot_ussc_logo.R
+++ R/plot_ussc_logo.R
@@ -10,3 +10,20 @@
 #' @author
 #' Zoe Meers
 #' @export
+
+
+plot_ussc_logo <- function(ggplot_object){
+  
+  get_png <- function(filename) {
+    grid::rasterGrob(png::readPNG(), interpolate = TRUE)
+  }
+  
+  logo <- get_png(system.file("img/logo.png", package = 'ussc'))
+  
+  logo_plot <- ggplot(mapping = aes(x = 0:1, y = 1)) +
+    theme_void() +
+    annotation_custom(grob = logo, xmin = 0.8, xmax = 1.1)
+  
+  gridExtra::grid.arrange(ggplot_object, logo_plot,  heights = c(.93,.1))
+  
+}
